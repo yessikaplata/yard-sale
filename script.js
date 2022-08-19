1d3517bdf6d275144d0d1221f336733877045d8e
Yessika Plata <yessikaliliana@gmail.com>
2022-08-19 08:44:25 -0500

    Integration with shopping cart

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-const navbarEmail = document.querySelector('.navbar-email')
-const navbarMobileMenuIcon = document.querySelector('.menu')
+const navbarEmail = document.querySelector('.navbar-email');
+const navbarMobileMenuIcon = document.querySelector('.menu');
+const navbarCartIcon = document.querySelector('.navbar-shopping-cart');
 
 navbarEmail.addEventListener('click', toggleDesktopMenu);
 navbarMobileMenuIcon.addEventListener('click', toggleMobileMenu);
+navbarCartIcon.addEventListener('click', toggleShoppingCart);
+
+
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart= document.querySelector('.order-container');
 
 function toggleDesktopMenu() {
-    const desktopMenu = document.querySelector('.desktop-menu');
+    hiddenHtmlElement(shoppingCart);
     toggleHtmlElement(desktopMenu);
-
 }
 
 function toggleMobileMenu() {
-    const mobileMenu = document.querySelector('.mobile-menu');
+    hiddenHtmlElement(shoppingCart);
     toggleHtmlElement(mobileMenu);
 }
 
+function toggleShoppingCart() {
+    hiddenHtmlElement(desktopMenu);
+    hiddenHtmlElement(mobileMenu);
+    toggleHtmlElement(shoppingCart);
+}
+
+
 function toggleHtmlElement(element) {
     element.classList.toggle('inactive');
+}
+
+function hiddenHtmlElement(element) {
+    element.classList.add('inactive');
 }
